2cf24d9f58d8b1aea57f605c83c3994e4e3b423a
System administrator <root@cognito-dev.novalocal>
2025-08-30 03:38:42 +0000

    feat: enable root autologin on my cloud machine and untested i3 configs

--- hosts/cognito-dev/configuration.nix
+++ hosts/cognito-dev/configuration.nix
@@ -1,3 +1,5 @@
+# /etc/nixos/hosts/cognito-dev/configuration.nix
+
 { config, pkgs, ... }:
 
 {
@@ -6,20 +8,28 @@
   networking.hostName = "cognito-dev";
 
   services.openssh.enable = true;
+  
+  services.getty.extraArgs = [ "--autologin" "root" ];
+
+  # Don't manage the boot loader on this cloud PC as the provider does that allegedly
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = false;
+
+  # Enable X11 and LightDM
+  services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+
+  # Use i3 as the window manager
+  services.xserver.windowManager.i3.enable = true;
 
   environment.systemPackages = with pkgs; [
     git
     vim
     htop
+    tmux   # for pane splitting test
   ];
-
-  # Enable flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Boot loader settings for VPS: don't install GRUB
-  boot.loader.grub.enable = false;
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = false;
 
   system.stateVersion = "23.11";
 }
+

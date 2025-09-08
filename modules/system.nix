b25a03a3f6efeaf84cc4503cce4888c96ba60362
Zi-Tong Deng <zitongcharliedeng2003@gmail.com>
2025-09-09 00:14:51 +0100

    docs

--- modules/system.nix
+++ modules/system.nix
@@ -52,6 +52,7 @@
     NIXOS_OZONE_WL = "1";
   };
 
+  # TODO add vm tools to develop NixOS in NixOS or figure out the dry-run NixOS changes with failsafe of rebooting without saving the changes to git. Dry running switch command i think it is called - add this action to the omnibar, with a NixOS icon. Same with the other common NixOS commands.
   environment.systemPackages = with pkgs; [
     waybar hyprpaper rofi-wayland
     obs-studio mangohud protonup

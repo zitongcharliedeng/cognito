e6814083efd636da1db2103d7459da1b5de50473
Zi-Tong Deng <zitongcharliedeng2003@gmail.com>
2025-09-09 14:21:34 +0100

    fix: update eww service installation target to ensure proper startup

--- modules/system.nix
+++ modules/system.nix
@@ -153,7 +153,7 @@ in
   # Eww daemon as user service to ensure overlays show reliably at login
   systemd.user.services.eww = {
     Unit.Description = "Eww daemon";
-    Install.WantedBy = [ "graphical-session.target" "default.target" ];
+    wantedBy = [ "default.target" ];
     Service = {
       ExecStart = "${pkgs.eww}/bin/eww -c /etc/eww daemon";
       Restart = "on-failure";

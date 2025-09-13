cf461f867a7fde7915b8f30ef30e1c2fdff1585c
Zi-Tong Deng <zitongcharliedeng2003@gmail.com>
2025-09-13 20:18:08 +0100

    feat: add XDG Portal configuration for improved application dialog support in Wayland

--- system-interface/01-cog/default.nix
+++ system-interface/01-cog/default.nix
@@ -13,6 +13,27 @@
     };
     programs.niri.enable = true;  # Wayland isn't a global toggle...
     # ... Wayland implicitly enabled by setting i.e. Niri as my window manager after signing in.
+    
+    # XDG Portal configuration for popups like Steam login, and maybe Niri to run right?
+    xdg.portal = {
+      enable = true;
+      config = {
+        common = {
+          default = ["gnome" "gtk"];
+        };
+        niri = {
+          default = ["gnome" "gtk"];
+          "org.freedesktop.impl.portal.ScreenCast" = ["gnome"];
+          "org.freedesktop.impl.portal.Screenshot" = ["gnome"];
+          "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+        };
+      };
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gnome
+        pkgs.xdg-desktop-portal-gtk
+      ];
+    };
+    
     environment.sessionVariables = {
         NIXOS_OZONE_WL = "1";  # Tells Chromium-based apps to use Wayland.
         WLR_NO_HARDWARE_CURSORS = "1";  # Fixes invisible/glitchy cursors i.e. in screenshots, etc.

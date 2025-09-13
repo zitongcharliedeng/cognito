3466f1ef13b8c284b37cae987f6b52e44b051de3
Zi-Tong Deng <zitongcharliedeng2003@gmail.com>
2025-09-13 20:26:22 +0100

    expfix: remove XDG Portal configuration and Niri keybindings from default.nix for cleaner setup

--- system-interface/01-cog/default.nix
+++ system-interface/01-cog/default.nix
@@ -14,50 +14,11 @@
     programs.niri.enable = true;  # Wayland isn't a global toggle...
     # ... Wayland implicitly enabled by setting i.e. Niri as my window manager after signing in.
     
-    # XDG Portal configuration for popups like Steam login, and maybe Niri to run right?
-    xdg.portal = {
-      enable = true;
-      config = {
-        common = {
-          default = ["gnome" "gtk"];
-        };
-        niri = {
-          default = ["gnome" "gtk"];
-          "org.freedesktop.impl.portal.ScreenCast" = ["gnome"];
-          "org.freedesktop.impl.portal.Screenshot" = ["gnome"];
-          "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
-        };
-      };
-      extraPortals = [
-        pkgs.xdg-desktop-portal-gnome
-        pkgs.xdg-desktop-portal-gtk
-      ];
-    };
     
     environment.sessionVariables = {
         NIXOS_OZONE_WL = "1";  # Tells Chromium-based apps to use Wayland.
         WLR_NO_HARDWARE_CURSORS = "1";  # Fixes invisible/glitchy cursors i.e. in screenshots, etc.
     };
 
-    environment.etc."niri/config.kdl".text = ''
-    input {
-        mod-key "Super"
-        keyboard {
-            xkb {
-                layout "us"
-            }
-        }
-    }
-
-    binds {
-        Mod+Space { spawn "toggle-omnibar-overlay"; }
-        Mod+Return { spawn "kitty"; }
-        Mod+Q { quit; }
-    }
-
-    layout {
-        gaps 8
-    }
-    '';
   };
 }

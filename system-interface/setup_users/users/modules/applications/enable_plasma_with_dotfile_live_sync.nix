01887418bf61fca43aa737db2bc8f86a162fdaa2
Zitong Deng <zitongcharliedeng2003@gmail.com>
2025-10-26 20:41:36 +0000

    fix: update plasma config export process and import paths

--- system-interface/setup_users/users/modules/applications/enable_plasma_with_dotfile_live_sync.nix
+++ system-interface/setup_users/users/modules/applications/enable_plasma_with_dotfile_live_sync.nix
@@ -1,29 +1,15 @@
 { self, config, pkgs, lib, plasmaManagerRc2nix, plasmaManagerHomeModule, ... }:
 
 let
-  # Lowest Common Ancestor for user Plasma KConfig (Plasmashell, KWin, etc.)
   MyLocalPlasmaConfigLCAFolder = "${config.home.homeDirectory}/.config";
-  # Target location in your monorepo for Plasma manager export (top-level .config)
   MyNixOSPlasmaConfigFolder = "${config.home.homeDirectory}/cognito/.config/plasma";
 in
 {
-  assertions = [
-    {
-      assertion = plasmaManagerRc2nix != null;
-      message = "plasmaManagerRc2nix must be provided (rc2nix from plasma-manager); no fallback is supported.";
-    }
-  ];
-
   systemd.user.services."export-plasma-dotfiles-to-nixconfig" = {
     Unit.Description = "Export Plasma config to monorepo as a Nix module using plasma-manager";
     Service = {
       Type = "oneshot";
-      ExecStart = [
-        "${pkgs.bash}/bin/bash" "-c"
-        ''
-          "${plasmaManagerRc2nix}" > "${MyNixOSPlasmaConfigFolder}/plasma.nix"
-        ''
-      ];
+      ExecStart = "${pkgs.bash}/bin/bash -lc ${lib.escapeShellArg "${plasmaManagerRc2nix} > ${MyNixOSPlasmaConfigFolder}/plasma.nix"}";
     };
     Install.WantedBy = [ "default.target" ];
   };
@@ -38,7 +24,7 @@ in
     Install.WantedBy = [ "default.target" ];
   };
 
-  imports = [ plasmaManagerHomeModule (import "${self}/.config/plasma/plasma.nix") ];
+  imports = [ plasmaManagerHomeModule (import "${MyNixOSPlasmaConfigFolder}/plasma.nix") ];
 }
 
 
